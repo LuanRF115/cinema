8716705bdda93794595612a8a0fa0cf36bcca6a0
Luan <luanlutii@gmail.com>
2025-05-30 22:18:22 -0300

    treino de php
    fatiar o código

---
+++ cabecalho.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CineFlix</title>
+    <link rel="stylesheet" href="filmes.css">
+</head>
+<body>
+    <header>
+        <img src="imagens/logo.png" class="logo" height="150">
+        <h1>CineFlix</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="index.php">Home</a>
+            <a href="index.php">Home</a>
+            <a href="index.php">Home</a>
+        </nav>
+    </header>
---
+++ rodape.php
@@ -0,0 +1,5 @@
+<footer class="rodape">
+        <hr>
+        <img src="imagens/logo.png"> 
+        <small><?php echo date ("Y");?></small>
+    </footer>
